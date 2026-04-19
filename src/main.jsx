b70cfeaaa357d0aa58a6149b90fab8b35a8d131a
Adwait Shrivastava <adwaitshrivastava9285@gmail.com>
2026-04-19 22:53:15 +0530

    Home Page Added: Crisper themed Udemy style home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import WatchPage from './pages/WatchPage/WatchPage'
+import Home from './pages/Home'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <WatchPage />
+    <Home />
   </React.StrictMode>,
 )
