1af6abc74ba59e934242c91d6466889250b35733
PK-1304 <pushkark1304@gmail.com>
2026-04-19 10:17:36 +0530

    main layout page

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+
+const Layout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="min-h-screen flex flex-col bg-[#0a0a0a] text-white">
+      <Header onMenuClick={() => setSidebarOpen(true)} />
+
+      <Sidebar
+        isOpen={sidebarOpen}
+        onClose={() => setSidebarOpen(false)}
+        userName="User"
+        initials="US"
+      />
+
+      <main className="flex-1 flex flex-col">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
